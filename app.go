cfdc65df5b9137ab43e3202da2876adf62125a1a
Illia <iliyaisd@gmail.com>
2023-07-20 14:40:39 -0400

    LittleJohn service

--- app.go
+++ app.go
@@ -7,6 +7,8 @@ import (
 	"github.com/iliyaisd/littlejohn/internal/datasource"
 )
 
+// Data source constants are used as just a demo of how actual different data source can be used in real project,
+// and the right one gets instantiated depending on the env var. 
 const (
 	DataSourceLocal = "local"
 	DataSourceYahoo = "yahoo"
